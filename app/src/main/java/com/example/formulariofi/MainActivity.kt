057b0856b358dbe86462a500f79c0f0f0c5f69a1
AbrahamRH <abrahamrzhz@gmail.com>
2023-04-24 18:29:15 -0600

    Adding the binding and the activity main

--- app/src/main/java/com/example/formulariofi/MainActivity.kt
+++ app/src/main/java/com/example/formulariofi/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.formulariofi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : MainActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     var btnNewProfile = findViewById<Button>(R.id.btnNewProfile)
+    fun newProfile(view: View) {
+
+    }
 }
