4b23bf67e59e2c1e3f61ca42e4cc695de0eee9ea
AbrahamRH <abrahamrzhz@gmail.com>
2023-04-26 13:16:57 -0600

    Adding the intent to the next activity with the button and the spinner

--- app/src/main/java/com/example/formulariofi/MainActivity.kt
+++ app/src/main/java/com/example/formulariofi/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.example.formulariofi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.*
 import com.example.formulariofi.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    private lateinit var binding : ActivityMainBinding
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var arrayCarreras : ArrayAdapter<CharSequence>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSpinner()
     }
 
-    var btnNewProfile = findViewById<Button>(R.id.btnNewProfile)
+    fun setSpinner() {
+        arrayCarreras = ArrayAdapter.createFromResource(
+            this, R.array.careers_array,
+            android.R.layout.simple_spinner_item
+        )
+        binding.spinnerCarrera.adapter = arrayCarreras
+        binding.spinnerCarrera.onItemSelectedListener = this
+    }
 
     fun newProfile(view: View) {
-
+        val intent = Intent(this, form::class.java)
+        startActivity(intent)
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
 }
