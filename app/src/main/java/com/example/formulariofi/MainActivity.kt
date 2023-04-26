8018a6bc351a34f09b96d68a1fdb4ed86dc1d04c
AbrahamRH <abrahamrzhz@gmail.com>
2023-04-25 23:44:07 -0600

    Adding the splash in the app

--- app/src/main/java/com/example/formulariofi/MainActivity.kt
+++ app/src/main/java/com/example/formulariofi/MainActivity.kt
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.example.formulariofi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : MainActivity
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     var btnNewProfile = findViewById<Button>(R.id.btnNewProfile)
+
     fun newProfile(view: View) {
 
     }
