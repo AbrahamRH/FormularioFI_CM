b53051162d0cca2898c9da10aa7b54f1d9c2afe1
AbrahamRH <abrahamrzhz@gmail.com>
2023-04-29 15:04:39 -0600

    Adding the images array and the implementation of selecting the career image

--- app/src/main/java/com/example/formulariofi/MainActivity.kt
+++ app/src/main/java/com/example/formulariofi/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.formulariofi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.*
 import com.example.formulariofi.databinding.ActivityMainBinding
@@ -14,12 +13,13 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var arrayCarreras: ArrayAdapter<CharSequence>
 
+    private var imgId = 0
     private var nombreIn = ""
     private var apellidosIn = ""
     private var correoIn = ""
     private var cuentaIn = ""
     private var carreraIn = ""
-    private var fechaNacIn = Triple<Int,Int,Int>(0,0,0)
+    private var fechaNacIn = Triple(0,0,0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +28,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         setSpinner()
         setDatePicker()
     }
-
     private fun setSpinner() {
         arrayCarreras = ArrayAdapter.createFromResource(
             this, R.array.careers_array,
@@ -49,23 +48,22 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             fechaNacIn = Triple(day, newMonth, year)
         }
     }
-
     fun newProfile(view: View) {
         nombreIn = binding.inputNombre.text.toString()
         apellidosIn = binding.inputApellidos.text.toString()
         correoIn = binding.inputEmail.text.toString()
         cuentaIn = binding.inputCuenta.text.toString()
 
-        Log.d("Bug: ",fechaNacIn.toString())
-
         val alumno = Alumno(nombreIn, apellidosIn, fechaNacIn, correoIn, cuentaIn, carreraIn)
         if (alumno.check()) {
-            val (isOk, errorMsg) = alumno.checkAttributes(resources);
+            val (isOk, errorMsg) = alumno.checkAttributes(resources)
             if (isOk) {
                 val intent = Intent(this, form::class.java)
                 val bundle = Bundle()
                 bundle.putParcelable("Alumno", alumno)
+                bundle.putInt("imgId", imgId)
                 intent.putExtras(bundle)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
                 startActivity(intent)
             } else {
                 Toast.makeText(
@@ -85,6 +83,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
         carreraIn = arrayCarreras.getItem(pos).toString()
+        imgId = pos
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
--- app/src/main/java/com/example/formulariofi/form.kt
+++ app/src/main/java/com/example/formulariofi/form.kt
@@ -1,36 +1,64 @@
 package com.example.formulariofi
 
-import android.annotation.SuppressLint
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import com.example.formulariofi.databinding.ActivityFormBinding
 
 class form : AppCompatActivity() {
 
     private lateinit var binding: ActivityFormBinding
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private val arrayImgCarreras  = setArrayCarreras()
+    private var imgId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setArrayCarreras()
         binding = ActivityFormBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val bundle = intent.extras
         if (bundle != null) {
-            val alumno = bundle.getParcelable("Alumno", Alumno::class.java)
+            val alumno = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ){
+                bundle.getParcelable("Alumno", Alumno::class.java)
+            } else {
+                bundle.getParcelable<Alumno>("Alumno")
+            }
             if (alumno != null) {
                 putContent(alumno)
             }
+
+            imgId = bundle.getInt("imgId")
+
         }
     }
 
     private fun putContent(alumno : Alumno) {
         binding.tvNombreCompleto.text = getString(R.string.nombreCompleto, alumno.nombre, alumno.apellidos)
-        binding.tvCarrera.text = alumno.carrera
+        binding.imgCarrera.setImageResource(arrayImgCarreras[imgId])
         binding.tvEdad.text = alumno.getYears().toString()
         binding.tvCorreo.text = alumno.correo
+    }
+
+    private fun setArrayCarreras() : ArrayList<Int> {
+        val arrayImgCarreras =  ArrayList<Int>(16)
+        arrayImgCarreras.add(0)
+        arrayImgCarreras.add(R.drawable.ing_espacial)
+        arrayImgCarreras.add(R.drawable.ing_civil)
+        arrayImgCarreras.add(R.drawable.ing_geomatica)
+        arrayImgCarreras.add(R.drawable.ing_ambiental)
+        arrayImgCarreras.add(R.drawable.ing_geofisica)
+        arrayImgCarreras.add(R.drawable.ing_geologica)
+        arrayImgCarreras.add(R.drawable.ing_petrolera)
+        arrayImgCarreras.add(R.drawable.ing_minas)
+        arrayImgCarreras.add(R.drawable.ing_computacion)
+        arrayImgCarreras.add(R.drawable.ing_electrica)
+        arrayImgCarreras.add(R.drawable.ing_telecomunicaciones)
+        arrayImgCarreras.add(R.drawable.ing_mecanica)
+        arrayImgCarreras.add(R.drawable.ing_industrial)
+        arrayImgCarreras.add(R.drawable.ing_mecatronica)
+        arrayImgCarreras.add(R.drawable.ing_biomedica)
+        return arrayImgCarreras
     }
 }
